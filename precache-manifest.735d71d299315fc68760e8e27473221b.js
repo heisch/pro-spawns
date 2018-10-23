09a96f2f1bb74e4fdb486e29e0b379d79fa32690
Nicola Heisch <nheisch@sensational.ch>
2018-10-23 10:18:27 +0200

    Updates

---
+++ precache-manifest.735d71d299315fc68760e8e27473221b.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/pro-spawns/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "2ce04483a90c0483ade6",
+    "url": "/pro-spawns/static/css/main.aaebc627.chunk.css"
+  },
+  {
+    "revision": "5e104bfc7d1a6177dead046715c21055",
+    "url": "/pro-spawns/static/media/HeadbuttSpawnData.5e104bfc.csv"
+  },
+  {
+    "revision": "bd022175ed110258ee44",
+    "url": "/pro-spawns/static/js/1.bd022175.chunk.js"
+  },
+  {
+    "revision": "5f28c4cb43efe5cc1acb",
+    "url": "/pro-spawns/static/js/runtime~main.5f28c4cb.js"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/pro-spawns/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/pro-spawns/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/pro-spawns/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/pro-spawns/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/pro-spawns/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/pro-spawns/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/pro-spawns/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "2ce04483a90c0483ade6",
+    "url": "/pro-spawns/static/js/main.2ce04483.chunk.js"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/pro-spawns/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/pro-spawns/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/pro-spawns/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/pro-spawns/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/pro-spawns/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/pro-spawns/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/pro-spawns/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/pro-spawns/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "139a7916d7040acbfc9de7ea2885224a",
+    "url": "/pro-spawns/static/media/RawSpawnData.139a7916.csv"
+  },
+  {
+    "revision": "ce1ee5a15af1f4d789c5dcca2ce2d04f",
+    "url": "/pro-spawns/static/media/RawWaterSpawnData.ce1ee5a1.csv"
+  },
+  {
+    "revision": "bd022175ed110258ee44",
+    "url": "/pro-spawns/static/css/1.d66a91b5.chunk.css"
+  },
+  {
+    "revision": "9034432f7738e0ef1a1bfdce8cf65553",
+    "url": "/pro-spawns/index.html"
+  }
+];
